22ed76d83ac51b86b3a23ecd479c73aa7f9da4e9
Chris Barry <2490974+ChrisW-B@users.noreply.github.com>
2023-02-27 10:08:14 -0500

    improve logging, build

--- src/functions/post.ts
+++ src/functions/post.ts
@@ -29,7 +29,12 @@ class Poster {
   };
 
   private uploadMedia = async (mediaUrl: string, description: string): Promise<string> => {
-    const uploadedMedia = await this.mastodonClient.post('/v2/media', { file: mediaUrl, description });
+    if (process.env['DEBUG'])
+      console.info(`Posting to ${process.env['MASTODON_API_URL'] ?? ''}/v2/media`);
+    const uploadedMedia = await this.mastodonClient.post('/v2/media', {
+      file: mediaUrl,
+      description,
+    });
     if (process.env['DEBUG']) {
       console.info('Uploaded media!');
       console.info(uploadedMedia.data);
@@ -40,8 +45,9 @@ class Poster {
   public run = async (event: APIGatewayEvent): Promise<void> => {
     try {
       const body = JSON.parse(event.body ?? '') as GhostPublishInfo;
-      console.log({ body });
+      console.log({ body: event.body });
       const currentPost = body?.post?.current;
+      console.log({ currentPost });
       if (currentPost) {
         const media = await this.uploadMedia(
           currentPost.feature_image,
